53614ea945d81f6d85f6ad8cd7c433e65c71ab77
Daniel Griffin <danielseangriffin@gmail.com>
2015-11-13 04:02:32 +0000

    updated tests, included travis

--- test/spec/test.js
+++ test/spec/test.js
@@ -4,7 +4,7 @@
   describe('Give it some context', function () {
     describe('maybe a bit more context here', function () {
       it('should run here few assertions', function () {
-
+        expect(1).to.equal(1);
       });
     });
   });
