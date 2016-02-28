88f1a786512f8905bcd27a0fcf3e2f127129a299
Daniel Griffin <danielseangriffin@gmail.com>
2016-02-28 04:14:22 +0000

    removed clutter, uglify js, optimised gruntfile, include package

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,10 +5,8 @@ module.exports = function (grunt) {
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
 
-  // Automatically load required Grunt tasks
-  require('jit-grunt')(grunt, {
-    useminPrepare: 'grunt-usemin'
-  });
+  // load all grunt-related tasks together instead of one-by-one
+  require('load-grunt-tasks')(grunt);
 
   // Configurable paths
   var config = {
@@ -26,31 +24,14 @@ module.exports = function (grunt) {
     watch: {
       js: {
         files: ['<%= config.srcScript %>/{,*/}*.js'],
-        tasks: 'babel',
-        options: {
-          livereload: '<%= connect.options.livereload %>'
-        }
+        tasks: 'babel'
       },
       gruntfile: {
         files: ['Gruntfile.js']
       },
       styles: {
         files: ['<%= config.app %>/styles/{,*/}*.css'],
-        tasks: [],
-        options: {
-          livereload: '<%= connect.options.livereload %>'
-        }
-      },
-      livereload: {
-        options: {
-          livereload: '<%= connect.options.livereload %>'
-        },
-        files: [
-          '<%= config.app %>/*.html',
-          '<%= config.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
-          '<%= config.app %>/manifest.json',
-          '<%= config.app %>/_locales/{,*/}*.json'
-        ]
+        tasks: []
       }
     },
 
@@ -67,80 +48,10 @@ module.exports = function (grunt) {
       }
     },
 
-    // Grunt server and debug server setting
-    connect: {
-      options: {
-        port: 9000,
-        livereload: 35729,
-        // change this to '0.0.0.0' to access the server from outside
-        hostname: 'localhost'
-      },
-      chrome: {
-        options: {
-          open: false,
-          base: [
-            '<%= config.app %>'
-          ]
-        }
-      },
-      test: {
-        options: {
-          open: false,
-          base: [
-            'test',
-            '<%= config.app %>'
-          ]
-        }
-      }
-    },
-
     // Empties folders to start fresh
     clean: {
-      dist: {
-        files: [{
-          dot: true,
-          src: [
-            '<%= config.dist %>/*',
-            '!<%= config.dist %>/.git*'
-          ]
-        }]
-      }
+      release: ['package']
     },
-
-    // Reads HTML for usemin blocks to enable smart builds that automatically
-    // concat, minify and revision files. Creates configurations in memory so
-    // additional tasks can operate on them
-    useminPrepare: {
-      options: {
-        dest: '<%= config.dist %>'
-      },
-      html: [
-        '<%= config.app %>/popup.html',
-        '<%= config.app %>/options.html'
-      ]
-    },
-
-    // Performs rewrites based on rev and the usemin prepare configuration
-    usemin: {
-      options: {
-        assetsDirs: ['<%= config.dist %>', '<%= config.dist %>/images']
-      },
-      html: ['<%= config.dist %>/{,*/}*.html'],
-      css: ['<%= config.dist %>/styles/{,*/}*.css']
-    },
-
-    // By default, your `index.html`'s <!-- Usemin block --> will take care of
-    // minification. These next options are pre-configured if you do not wish
-    // to use the Usemin blocks.
-     cssmin: {
-       dist: {
-         files: {
-           '<%= config.dist %>/styles/main.css': [
-             '<%= config.app %>/styles/{,*/}*.css'
-           ]
-         }
-       }
-     },
 
     // Copies remaining files to places other tasks can use
     copy: {
@@ -158,11 +69,21 @@ module.exports = function (grunt) {
               'scripts/{,*/}*.js',
               '{,*/}*.html',
               'styles/{,*/}*.css',
-              '_locales/{,*/}*.json',
               'manifest.json'
             ]
           }
         ]
+      }
+    },
+
+    uglify: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= config.dist %>/scripts',
+          src: ['*.js', '!*.min.js'],
+          dest: '<%= config.dist %>/scripts/'
+        }]
       }
     },
 
@@ -172,12 +93,7 @@ module.exports = function (grunt) {
         options: {
           buildnumber: true,
           indentSize: 2,
-          background: {
-            target: 'scripts/background.js',
-            exclude: [
-              'scripts/chromereload.js'
-            ]
-          }
+          background: {}
         },
         src: '<%= config.app %>',
         dest: '<%= config.dist %>'
@@ -204,18 +120,15 @@ module.exports = function (grunt) {
   });
 
   grunt.registerTask('build', [
-    'clean:dist',
-    'babel',
-    'useminPrepare',
-    'cssmin',
     'copy:dist',
-    'usemin',
-    'compress'
+    'babel',
+    'uglify'
   ]);
 
   grunt.registerTask('release', [
+    'clean:release',
     'chromeManifest:dist',
-    'build'
+    'compress'
   ]);
 
   grunt.registerTask('default', [
