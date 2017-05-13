f60756d05b41909adf23751677baedc0830da7f2
Daniel Griffin <danielseangriffin@gmail.com>
2017-05-13 14:03:08 +0100

    remove typings folder, install via npm

--- typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="globals/chrome/index.d.ts" />
-/// <reference path="globals/datatables.net/index.d.ts" />
-/// <reference path="globals/filesystem/index.d.ts" />
-/// <reference path="globals/filewriter/index.d.ts" />
-/// <reference path="globals/jquery/index.d.ts" />
